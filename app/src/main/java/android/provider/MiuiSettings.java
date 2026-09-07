83885523e6de231b5e2f932c95f2d8c1cb97a7cd
pbzin <pbzinwindows@gmail.com>
2026-09-06 23:10:54 -0300

    Fix MiuiSettings boolean compatibility

--- app/src/main/java/android/provider/MiuiSettings.java
+++ app/src/main/java/android/provider/MiuiSettings.java
@@ -14,6 +14,10 @@ public final class MiuiSettings {
             return Settings.System.getInt(resolver, name, def);
         }
 
+        public static boolean getBoolean(ContentResolver resolver, String name, boolean def) {
+            return Settings.System.getInt(resolver, name, def ? 1 : 0) != 0;
+        }
+
         public static String getString(ContentResolver resolver, String name) {
             return Settings.System.getString(resolver, name);
         }
